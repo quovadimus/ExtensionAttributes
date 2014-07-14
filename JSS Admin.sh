34df36a86f631083bdef54fc6223fab4df526694
Jennfier Unger <jennifer_unger@navyfederal.org>
2014-07-14 10:38:53 -0400

    changed to generic username

--- JSS Admin.sh
+++ JSS Admin.sh
@@ -2,11 +2,12 @@
 #
 ############################################################################
 #
-#Extension Attribute searches for jssadmin hidden account
+#Extension Attribute searches for hidden user account
 #
+#by Jennifer Unger
 ############################################################################
 
-if [[ $(dscl . list /Users UniqueID | awk '$2 < 501 {print $1}' | grep -i "^jssadmin$") ]]; then
+if [[ $(dscl . list /Users UniqueID | awk '$2 < 501 {print $1}' | grep -i "^nameofhiddenaccount$") ]]; then
       echo "<result>Present</result>"
 else
       echo "<result>Not Present</result>"
